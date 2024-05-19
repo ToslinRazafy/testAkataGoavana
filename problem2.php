6558a5f3939ce7eb8289283e066c1ce89f39c198
ToslinRazafy <tosyrazafitsotra@gmail.com>
2024-05-19 18:06:15 +0200

    problem2.php

--- problem2.php
+++ problem2.php
@@ -1,30 +1,41 @@
 <?php
-    function pairProgramming(array $experiences, bool $isMostExperienced): array {
-        $tab = [];
-        $max =$experiences[0];
-        $max2 = 2;
-        for($i = 0; $i < count($experiences); $i++){
-            if($isMostExperienced){
-                if($experiences[$i] > $max){
-                    $max = $experiences[$i];
-                    $tab[] = $i;
-                    $max2 = $i;
-                    if($tab[0] > $max2){
-                        $max2 = $tab[0];
-                        $tab[1] = $max2;
-                    }
-                }
-            }
-        }
-        return $tab;
-    }
+    function pairProgramming(array $experiences, bool $isMostExperienced): array{
+        $tmp = $experiences;
+        $tab = [];
+        if($isMostExperienced){
+            sort($experiences, SORT_NUMERIC);
+            $tab[] = $experiences[count($experiences)-2];
+            $tab[] = $experiences[count($experiences)-1];
+            for($i = 0; $i < count($tmp); $i++){
+                if($tab[0] === $tmp[$i]){
+                    $tab[0] = $i;
+                }
+                if($tab[1] === $tmp[$i] ){
+                    $tab[1] = $i;
+                }
+            }
+       }else{
+            sort($experiences, SORT_NUMERIC);
+            $tab[] = $experiences[0];
+            $tab[] = $experiences[1];
+            for($i = 0; $i < count($tmp); $i++){
+                if($tab[0] === $tmp[$i]){
+                    $tab[0] = $i;
+                }
+                if($tab[1] === $tmp[$i] ){
+                    $tab[1] = $i;
+                }
+            }
+       }
+       return $tab;
+    }
 
-    $resultat = pairProgramming([1, 4, 2, 3, 8, 3], true);
-    echo "[";
-    for($i = 0; $i < count($resultat); $i++){
-        echo $resultat[$i];
-        if($i === 0 ){
-            echo ",";
-        }
-    }
-    echo "]";
+    $tab = pairProgramming([1, 4, 2, 3, 8, 10, 9], true);
+    echo "[";
+    for($i = 0; $i < count($tab); $i++){
+        echo $tab[$i];
+        if($i === 0){
+            echo ",";
+        }
+    }
+    echo "]";
