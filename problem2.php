028d7bf118b7dbe8224fcf8109ee609b31f8977c
ToslinRazafy <tosyrazafitsotra@gmail.com>
2024-05-19 14:31:55 +0200

    problem2.php

--- problem2.php
+++ problem2.php
@@ -1,22 +1,30 @@
 <?php
-    function pairProgramming(array $experiences, bool $isMostExperienced): array {
-        $tab = [];
-        $max =$experiences[0];
-        $max2 = 2;
-        for($i = 0; $i < count($experiences); $i++){
-            if($isMostExperienced){
-                if($experiences[$i] > $max){
-                    $max = $experiences[$i];
-                    $tab[] = $i;
-                    $max2 = $i;
-                    if($tab[0] > $max2){
-                        $max2 = $tab[0];
-                        $tab[1] = $max2;
-                    }
-                }
-            }
-        }
-        return $tab;
-    }
+    function pairProgramming(array $experiences, bool $isMostExperienced): array {
+        $tab = [];
+        $max =$experiences[0];
+        $max2 = 2;
+        for($i = 0; $i < count($experiences); $i++){
+            if($isMostExperienced){
+                if($experiences[$i] > $max){
+                    $max = $experiences[$i];
+                    $tab[] = $i;
+                    $max2 = $i;
+                    if($tab[0] > $max2){
+                        $max2 = $tab[0];
+                        $tab[1] = $max2;
+                    }
+                }
+            }
+        }
+        return $tab;
+    }
 
-    // var_dump(pairProgramming([1, 4, 2, 3, 8, 3], true));
+    $resultat = pairProgramming([1, 4, 2, 3, 8, 3], true);
+    echo "[";
+    for($i = 0; $i < count($resultat); $i++){
+        echo $resultat[$i];
+        if($i === 0 ){
+            echo ",";
+        }
+    }
+    echo "]";
